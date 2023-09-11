c67f0fbad9cb119038e3e32d4b5810602d1e711e
farizftrhmn <144640362+farizftrhmn@users.noreply.github.com>
2023-09-11 09:30:06 +0700

    Update and rename MenghitungLuasPersegiPanjang.cpp to MenghitungLuasPersegi.cpp

---
+++ MenghitungLuasPersegi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () {
+int l,s;
+cout<<"MENGHITUNG LUAS PERSEGI \n";
+cout<<"masukkan sisi: ";
+cin>>s;
+
+
+l=s*s;
+cout<<"hasil luas persegi adalah: "<<l<<endl;
+return 0;
+}
